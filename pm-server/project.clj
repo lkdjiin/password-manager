80022e2c881f68e7c6083aedd28eb165b02fd389
Xavier Nayrac <xavier.nayrac@gmail.com>
2017-04-02 14:01:06 +0200

    Fix license in project.clj

--- pm-server/project.clj
+++ pm-server/project.clj
@@ -1,8 +1,8 @@
 (defproject pm-server "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU General Public License v3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.0-RC1"]
                  [org.clojure/java.jdbc "0.7.0-alpha3"]
