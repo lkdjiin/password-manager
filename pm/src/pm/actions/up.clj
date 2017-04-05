c9b018bf55815b8c48d7e4ed1b21ef770acbfca3
Xavier Nayrac <xavier.nayrac@gmail.com>
2017-04-05 09:32:37 +0200

    Extract server.clj

--- pm/src/pm/actions/up.clj
+++ pm/src/pm/actions/up.clj
@@ -1,14 +1,10 @@
 (ns pm.actions.up
-  (:require [pm.input :as input]))
+  (:require [pm.input :as input]
+            [pm.server :refer :all]))
 
 (use '[clojure.java.shell :only [sh]])
 
-; FIXME Make those paths dynamic!
-(def servername "/home/xavier/perso/password-manager/pm-server/target/uberjar/pm-server-0.1.0-SNAPSHOT-standalone.jar")
-(def serverlog "/home/xavier/.pm/server.log")
-
 (def unlocked  "Unlocked for 5 minutes")
-(def already-up "Already up and running")
 
 (defn launch-server
   [pwd]
---
+++ pm/src/pm/server.clj
@@ -0,0 +1,11 @@
+(ns pm.server)
+
+; FIXME Make this path dynamic!
+(defn servername
+  []
+  "/home/xavier/perso/password-manager/pm-server/target/uberjar/pm-server-0.1.0-SNAPSHOT-standalone.jar")
+
+; FIXME Make this path dynamic!
+(defn serverlog
+  []
+  "/home/xavier/.pm/server.log")
